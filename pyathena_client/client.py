bbfb546e9e6122c8af7968ff1ad6b4d2606f88c5
Shu Ming Peh <>
2022-04-22 16:04:43 +1000

    change pyathena functions to be just initialise and create_msck_repair_table, borrows from utils function

--- pyathena_client/client.py
+++ pyathena_client/client.py
@@ -1,12 +1,16 @@
+from __future__ import annotations
+
 import datetime
 import os
 
-import pandas as pd
 import pyathena
 from pyathena import connect
 from pyathena.pandas.cursor import PandasCursor
 
 from core.config import settings
+from core.utils import format_sql_create_schema
+from core.utils import format_sql_repair_table
+from core.utils import read_sql
 
 
 class PyAthenaClient:
@@ -16,35 +20,21 @@ class PyAthenaClient:
     """
 
     def __init__(self):
-        self.engine_pd = self._connect()
-        self.engine = self._connect_faster()
+        self.engine = self._connect()
 
     def _connect(self) -> pyathena.connection.Connection:
         """
-        create a pyathena connection
+        create a pyathena connection with pandas cursor
+        this queries athena queries much faster
         Returns
         -------
         pyathena.connection.Connection
             pyathena connection engine
         """
-        today_date = pd.to_datetime(datetime.datetime.now()).strftime("%Y-%m-%d")
+        today_date = (datetime.datetime.now()).strftime("%Y-%m-%d")
 
         connection = connect(
             s3_staging_dir=f"{settings.S3_ATHENA_QUERY_DIRECTORY}query_{today_date}",
-            region_name="ap-southeast-2",
-            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-        )
-        return connection
-
-    def _connect_faster(self) -> pyathena.connection.Connection:
-        today_date = pd.to_datetime(datetime.datetime.now()).strftime("%Y-%m-%d")
-
-        connection = connect(
-            s3_staging_dir=(
-                """s3://au-com-hipages-offline-feature-store/athena_queries/"""
-                f"""query_{today_date}"""
-            ),
             region_name="ap-southeast-2",
             aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
             aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
@@ -52,14 +42,41 @@ class PyAthenaClient:
         )
         return connection
 
-    def create_table(self, query: str) -> int:
-        # create table if does not exist
-        pd.read_sql(sql=query, con=self.engine_pd)
+    def create_msck_repair_table(
+        self, create_raw_query: str, repair_raw_query: str, yaml_schema_file_path: str
+    ) -> int:
+        """
+        create table and msck repair table in athena with pyathena connection
 
-        return 0
+        Parameters
+        ----------
+        create_raw_query : str
+            raw query string to create table in athena
+        repair_raw_query : str
+            raw query string to repair table in athena
+        yaml_schema_file_path : str
+            yaml file path for schema of table to be created and repaired
 
-    def msck_repair_table(self, query: str) -> int:
-        # add missing partitions
-        pd.read_sql(sql=query, con=self.engine_pd)
+        Returns
+        -------
+        int
+            non exit function value if successful
+        """
 
-        return 0
+        create_schema_query_raw = read_sql(file_path=create_raw_query)
+        repair_table_query_raw = read_sql(file_path=repair_raw_query)
+
+        create_schema_query, table_name = format_sql_create_schema(
+            sql=create_schema_query_raw, yaml_file_path=yaml_schema_file_path
+        )
+
+        repair_table_query = format_sql_repair_table(
+            sql=repair_table_query_raw, table_name=table_name
+        )
+
+        try:
+            self.engine.cursor().execute(create_schema_query)
+            self.engine.cursor().execute(repair_table_query)
+            return 0
+        except Exception:
+            return 1
