bdb95181f3d3938efbb8a254c3010f635a75c597
Shu Ming Peh <>
2022-04-26 20:55:45 +1000

    test files for sagemaker client

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 import pytest
@@ -9,3 +10,65 @@ def aws_credentials():
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"  # pragma: allowlist secret
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"  # pragma: allowlist secret
     os.environ["AWS_DEFAULT_REGION"] = "ap-southeast-2"  # pragma: allowlist secret
+
+
+@pytest.fixture(autouse=True)
+def get_rejected_list_package():
+    list_model_packages_response = {
+        "ModelPackageSummaryList": [
+            {
+                "ModelPackageName": "churn-test",
+                "ModelPackageArn": "test",
+                "CreationTime": datetime.datetime(2022, 1, 1),
+                "ModelPackageStatus": "Completed",
+                "ModelApprovalStatus": "Rejected",
+            }
+        ]
+    }
+    return list_model_packages_response
+
+
+@pytest.fixture(autouse=True)
+def get_approved_list_package():
+    list_model_packages_response = {
+        "ModelPackageSummaryList": [
+            {
+                "ModelPackageName": "churn-test",
+                "ModelPackageArn": "test",
+                "CreationTime": datetime.datetime(2022, 1, 1),
+                "ModelPackageStatus": "Completed",
+                "ModelApprovalStatus": "Approved",
+            }
+        ]
+    }
+    return list_model_packages_response
+
+
+@pytest.fixture(autouse=True)
+def get_describe_model_package_response():
+    describe_model_package_response = {
+        "ModelPackageName": "churn-test",
+        "ModelPackageArn": "test",
+        "CreationTime": datetime.datetime(2022, 1, 1),
+        "ModelPackageStatus": "Completed",
+        "ModelPackageStatusDetails": {
+            "ValidationStatuses": [
+                {
+                    "Name": "whatever",
+                    "Status": "Completed",
+                    "FailureReason": "string",
+                },
+            ],
+        },
+        "InferenceSpecification": {
+            "Containers": [
+                {
+                    "ModelDataUrl": "whatever/model_testing.tar.gz",
+                    "Image": "urltest",
+                }
+            ],
+            "SupportedContentTypes": ["str"],
+            "SupportedResponseMIMETypes": ["str"],
+        },
+    }
+    return describe_model_package_response
---
+++ tests/data_ml_utils/boto3_botocore_client/test_client_sagemaker.py
@@ -0,0 +1,368 @@
+import filecmp
+
+import boto3
+import botocore
+from botocore.exceptions import ParamValidationError
+from botocore.exceptions import StubAssertionError
+from botocore.stub import ANY
+from botocore.stub import Stubber
+from mock import patch
+from moto import mock_s3
+from moto import mock_sagemaker
+
+from data_ml_utils.boto3_botocore_client.client_sagemaker import AwsSagemakerServices
+
+orig = botocore.client.BaseClient._make_api_call
+
+
+class TestAWSServices:
+    """test class for AWS session"""
+
+    def test_boto3_session(self, aws_credentials):
+        """
+        test function to initialise a boto3 session
+
+        Parameters
+        ----------
+        aws_credientials
+            inherits the aws creds when invoking aws functions
+
+        Returns
+        -------
+        assert
+            if class session is boto3.session.Session
+        """
+        bc_session_patch = patch("boto3.session.Session")
+        bc_session_cls = bc_session_patch.start()
+        loader = bc_session_cls.return_value.get_component.return_value
+        loader.data_path = ""
+        patch_global_session = patch("boto3.DEFAULT_SESSION")
+        patch_global_session.start()
+
+        assert bc_session_cls == boto3.session.Session
+
+    def test_get_model_uri_from_aws_model_registry(
+        self,
+        aws_credentials,
+        get_describe_model_package_response,
+        get_approved_list_package,
+    ):
+        """
+        test function to retrieve model uri from sagemaker model registry
+
+        Parameters
+        ----------
+        aws_credientials
+            inherits the aws creds when invoking aws functions
+        get_describe_model_package_response
+            sagemaker describe_model_package response
+        get_approved_list_package
+            sagemaker list_model_packages response, approved
+
+        Returns
+        -------
+        assert
+            if file name is the same
+        """
+
+        mock = mock_sagemaker()
+        mock.start()
+        aws_client = AwsSagemakerServices()
+
+        stubber = Stubber(aws_client.client_sagemaker)
+
+        expected_params = {"ModelPackageGroupName": ANY}
+        stubber.add_response(
+            "list_model_packages", get_approved_list_package, expected_params
+        )
+        expected_params_ = {"ModelPackageName": ANY}
+        stubber.add_response(
+            "describe_model_package",
+            get_describe_model_package_response,
+            expected_params_,
+        )
+        stubber.activate()
+
+        test_uri, test_file = aws_client.get_model_uri_from_aws_model_registry(
+            "churn-test"
+        )
+
+        mock.stop()
+
+        assert test_file == "model_testing.tar.gz"
+
+    def test_get_model_uri_from_aws_model_registry_error_approved(
+        self,
+        aws_credentials,
+        get_describe_model_package_response,
+        get_rejected_list_package,
+    ):
+        """
+        test function to retrieve model uri from sagemaker model registry
+
+        Parameters
+        ----------
+        aws_credientials
+            inherits the aws creds when invoking aws functions
+        get_describe_model_package_response
+            sagemaker describe_model_package response
+        get_rejected_list_package
+            sagemaker list_model_packages response, rejected
+
+        Returns
+        -------
+        assert
+            captures ParamValidationError
+        """
+
+        mock = mock_sagemaker()
+        mock.start()
+        aws_client = AwsSagemakerServices()
+
+        stubber = Stubber(aws_client.client_sagemaker)
+
+        expected_params = {"ModelPackageGroupName": ANY}
+        stubber.add_response(
+            "list_model_packages", get_rejected_list_package, expected_params
+        )
+        expected_params_ = {"ModelPackageName": ANY}
+        stubber.add_response(
+            "describe_model_package",
+            get_describe_model_package_response,
+            expected_params_,
+        )
+        stubber.activate()
+
+        try:
+            _, _ = aws_client.get_model_uri_from_aws_model_registry("churn-test")
+        except ParamValidationError:
+            message = "ModelGroupName does not have approved status"
+
+        mock.stop()
+
+        assert message == "ModelGroupName does not have approved status"
+
+    def test_get_model_uri_from_aws_model_registry_invalid_modelname(
+        self,
+        aws_credentials,
+        get_describe_model_package_response,
+        get_approved_list_package,
+    ):
+        """
+        test function to retrieve model uri from sagemaker model registry
+
+        Parameters
+        ----------
+        aws_credientials
+            inherits the aws creds when invoking aws functions
+        get_describe_model_package_response
+            sagemaker describe_model_package response
+        get_approved_list_package
+            sagemaker list_model_packages response, approved
+
+        Returns
+        -------
+        assert
+            captures ParamValidationError
+        """
+
+        mock = mock_sagemaker()
+        mock.start()
+        aws_client = AwsSagemakerServices()
+
+        stubber = Stubber(aws_client.client_sagemaker)
+
+        expected_params = {"ModelPackageGroupName": "churn-test"}
+        stubber.add_response(
+            "list_model_packages", get_approved_list_package, expected_params
+        )
+        expected_params_ = {"ModelPackageName": "churn-test"}
+        stubber.add_response(
+            "describe_model_package",
+            get_describe_model_package_response,
+            expected_params_,
+        )
+        stubber.activate()
+
+        try:
+            _, _ = aws_client.get_model_uri_from_aws_model_registry("churn-prod")
+        except StubAssertionError:
+            message = "incorrect model group name"
+
+        mock.stop()
+
+        assert message == "incorrect model group name"
+
+    def test_unzip_targz_file(self, aws_credentials):
+        """
+        test function to unzip tar gz file
+
+        Parameters
+        ----------
+        aws_credientials
+            inherits the aws creds when invoking aws functions
+
+        Returns
+        -------
+        assert
+            if returns non exit function value
+        """
+
+        mock = mock_s3()
+        mock.start()
+        aws_client = AwsSagemakerServices()
+        # create bucket
+        aws_client.client_s3.create_bucket(
+            Bucket="au-com-hipages-radagast",
+            CreateBucketConfiguration={"LocationConstraint": "ap-southeast-2"},
+        )
+
+        aws_client.client_s3_resource.meta.client.upload_file(
+            "tests/dummy.tar.gz",
+            "au-com-hipages-radagast",
+            "scratchpad/muriel/models/2022_01_01/dummy.tar.gz",
+        )
+
+        response_result = aws_client.unzip_targz_file(
+            "dummy.tar.gz",
+            "au-com-hipages-radagast",
+            "scratchpad/muriel/models/2022_01_01/dummy.tar.gz",
+            "scratchpad/muriel/models/2022_01_01",
+        )
+
+        mock.stop()
+
+        assert response_result == 0
+        assert filecmp.cmp("dummy.tar.gz", "tests/dummy.tar.gz")
+
+    def test_upload_retrained_model_s3(self, aws_credentials):
+        """
+        test function to upload retrained model to s3
+
+        Parameters
+        ----------
+        aws_credientials
+            inherits the aws creds when invoking aws functions
+
+        Returns
+        -------
+        assert
+            if model file name is as expected
+        """
+        mock = mock_s3()
+        mock.start()
+        aws_client = AwsSagemakerServices()
+
+        aws_client.client_s3.create_bucket(
+            Bucket="test-bucket",
+            CreateBucketConfiguration={"LocationConstraint": "ap-southeast-2"},
+        )
+
+        test_file = aws_client.upload_retrained_model_s3(
+            "2022_01_01", "test-bucket", "", "model_muriel"
+        )
+
+        mock.stop()
+
+        assert test_file == "model_muriel_2022_01_01.tar.gz"
+
+    def test_create_model_package_version_error(self, aws_credentials):
+        """
+        test function to create model package version in AWS model registry
+
+        Parameters
+        ----------
+        aws_credientials
+            inherits the aws creds when invoking aws functions
+
+        Returns
+        -------
+        assert
+            if response is as expected
+        """
+        mock = mock_sagemaker()
+        mock.start()
+        aws_client = AwsSagemakerServices()
+
+        retrained_model_metrics_dict = {
+            "accuracy_top_10": 1.0,
+        }
+
+        current_model_metrics_dict = {
+            "accuracy_top_10": 0.9,
+        }
+
+        stubber = Stubber(aws_client.client_sagemaker)
+
+        create_model_package_response = {"ModelPackageArn": "test"}
+        expected_params = {"ModelPackageGroupName": ANY}
+        stubber.add_response(
+            "create_model_package", create_model_package_response, expected_params
+        )
+
+        try:
+            aws_client.create_model_package_version(
+                "churn-test",
+                "model_123.tar.gz",
+                retrained_model_metrics_dict,
+                current_model_metrics_dict,
+                "test-bucket",
+                "testing",
+            )
+        except Exception:
+            message = "error message"
+
+        mock.stop()
+
+        assert message == "error message"
+
+    def test_create_model_package_version(self, aws_credentials):
+        """
+        test function to create model package version in AWS model registry
+
+        Parameters
+        ----------
+        aws_credientials
+            inherits the aws creds when invoking aws functions
+
+        Returns
+        -------
+        assert
+            if response is as expected
+        """
+        mock = mock_sagemaker()
+        mock.start()
+        aws_client = AwsSagemakerServices()
+
+        current_retrained_model_metrics_dict = {
+            "retrained_top_10_accuracy": "1.0",
+            "current_top_10_accuracy": "1.0",
+        }
+
+        stubber = Stubber(aws_client.client_sagemaker)
+
+        create_model_package_response = {"ModelPackageArn": "test"}
+        expected_params = {
+            "CustomerMetadataProperties": ANY,
+            "InferenceSpecification": ANY,
+            "ModelApprovalStatus": ANY,
+            "ModelPackageDescription": ANY,
+            "ModelPackageGroupName": ANY,
+        }
+        stubber.add_response(
+            "create_model_package", create_model_package_response, expected_params
+        )
+        stubber.activate()
+
+        response = aws_client.create_model_package_version(
+            "churn-test",
+            "model_123.tar.gz",
+            current_retrained_model_metrics_dict,
+            "test-bucket/",
+            "testing",
+            "description",
+        )
+
+        mock.stop()
+
+        assert response == 0
